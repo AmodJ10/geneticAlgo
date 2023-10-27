862e1d3a1fe4c3cea91da39bb297e0373780ba83
JOSHI AMOD SANDEEP <121449626+AmodJ10@users.noreply.github.com>
2023-10-27 09:53:17 +0530

    Changed MatingPool

--- population.py
+++ population.py
@@ -2,7 +2,7 @@ import DNA
 import random
 
 population = []
-totalPopulation = 100
+totalPopulation = 10
 mutationRate = 0
 
 def setup(population,totalPopulation):
@@ -16,59 +16,44 @@ def draw():
     
     offsprings = []
     for i in range(totalPopulation):
-            mother = population.getParent()
-            father = population.getParent()
+            mother = getParent(population)
+            father =getParent(population)
 
             while(mother == father):
-                father = self.getParent()
-            offspring1, offspring2 = self.getCrossover(mother,father)
+                father = getParent(population)
+            offspring1, offspring2 = population(mother,father)
             offsprings.append(offspring1)
             offsprings.append(offspring2)
-        offsprings += self.individuals
-        offsprings.sort(key = lambda x : x.getFitness(),reverse=False)
+    offsprings += population
+    offsprings.sort(key = lambda x : x.fitness(),reverse=False)
+    population = offsprings[:100]
     
-    for i in range(totalPopulation):
     
-        a = random.randint(0,len(matingPool)-1)
-        b = random.randint(0,len(matingPool)-1)    
-
-        while b!=a:
-            b = random.randint(0,len(matingPool)-1)    
-
-        parentA = matingPool[a]
-        parentB = matingPool[b]
-
-        
-        child = parentA.crossOver(parentB)
-        child.mutate(mutationRate)
-        
-        population.append(child)
-        
-def getParent(self):
+def getParent(population):
     if random.random() > 0.5:
         # Tournament Selection
-        return population.tournamentSelection()
+        return tournamentSelection(population)
     else : 
         # Biased Random Selection
-        return population.biasedRandomSelection()
+        return biasedRandomSelection(population)
     
-def tournamentSelection(self):
-        candidate1 = self.individuals[random.randint(0,self.population-1)]
-        candidate2 = self.individuals[random.randint(0,self.population-1)]
+def tournamentSelection(population):
+        candidate1 = population[random.randint(0,totalPopulation-1)]
+        candidate2 =population[random.randint(0,totalPopulation-1)]
 
         while(candidate1 == candidate2):
-            candidate2 = self.individuals[random.randint(0,self.population-1)]
+            candidate2 = population[random.randint(0,totalPopulation-1)]
         
-        if candidate1.getFitness() < candidate2.getFitness():
+        if candidate1.fitness < candidate2.fitness:
             return candidate1
         else:
             return candidate2
     
-def biasedRandomSelection(self):
+def biasedRandomSelection(population):
     fitnessSum = 0
-    for i in self.individuals:
-        fitnessSum += i.getFitness()
-    proportions = [fitnessSum/i.getFitness() for i in self.individuals]
+    for i in population:
+        fitnessSum += i.fitness
+    proportions = [fitnessSum/i.fitness for i in population]
     proportionsSum = sum(proportions)
     normalizedProportions = [p/proportionsSum for p in proportions]
 
@@ -80,20 +65,23 @@ def biasedRandomSelection(self):
 
     selectedValue = random.random()
 
-    for i in range(self.population):
+    for i in range(totalPopulation):
         if selectedValue <= cumulativeProportions[i]:
-            return self.individuals[i]
-    return self.individuals[random.randint(0,self.population - 1)] # Redundan
+            return population[i]
+    return population[random.randint(0,totalPopulation - 1)] # Redundand
 
         
 setup(population,totalPopulation)
 
-while True:
-    fitness_array = [i.fitness for i in population]
-    if max(fitness_array) >= 44:
-        break
-    else:
-        print(population[fitness_array.index(max(fitness_array))])
+while population[0].fitness <= 44 :
+    for i in range(totalPopulation):
+        print("".join(population[i].genes))
+    draw()
+    
+
+   
+   
+   
 
 
 
